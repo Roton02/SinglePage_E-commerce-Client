d41b68be265803c1755d7002762aa2da48980679
Roton02 <rph645102@gmail.com>
2024-08-15 02:10:16 +0600

    firebase initialization complete

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -7,7 +7,7 @@ AOS.init();
 
 const Root = () => {
   return (
-    <div className="mt-4 bg-gradient-to-r from-[#00dbde] to-[#fc00ff] bg-transparent">
+    <div className="mt-4 bg-gradient-to-r from-[#81d3e1]  to-[#ef91ec]">
       <Navbar></Navbar>
       <div className=" max-w-7xl mx-auto min-h-calc">
         <Outlet></Outlet>
