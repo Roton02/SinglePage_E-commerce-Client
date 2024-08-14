9a958d4dd7e9c587bdc86313fa07f3ee94b54581
Roton02 <rph645102@gmail.com>
2024-08-14 22:55:20 +0600

    Create : create a navbar

--- src/Shared/Navber/Navbar.jsx
+++ src/Shared/Navber/Navbar.jsx
@@ -1,163 +1,171 @@
-
 import "./Navbar.css";
-import 'animate.css';
-import { useContext } from "react";
+import "animate.css";
+import { useContext, useState } from "react";
 import { AuthContext } from "../../ContextProvider/ContextProvider";
 import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
   const { Logout, user } = useContext(AuthContext);
   console.log(user);
   return (
-    <nav className="navbar z-[100]  w-full  bg-base-100">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5 "
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+    <div>
+      <nav className="relative bg-gradient-to-r from-[#F9F3F0] from-10% via-[#FCE7DC] via-30% to-[#F9F3F0] to-90% dark:bg-gradient-to-r dark:from-[#f2f2d8] dark:from-10% dark:via-[#FCE7DC] dark:via-30% dark:to-[#fae1d4] ">
+        <div className="container py-4 mx-auto max-w-7xl px-4 lg:px-0">
+          <div className="lg:flex lg:items-center lg:justify-between">
+            <div className="flex items-center justify-between">
+              <Link to="/">
+                <img
+                  className="w-24 md:w-32 lg:w-40"
+                  src="https://i.ibb.co/L0v6r6Z/logo-finel-removebg-preview.png"
+                  alt=""
+                />
+              </Link>
+              <div className="flex lg:hidden">
+                <button
+                  onClick={() => setIsOpen(!isOpen)}
+                  type="button"
+                  className=""
+                  aria-label="toggle menu"
+                >
+                  {isOpen ? (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M6 18L18 6M6 6l12 12"
+                      />
+                    </svg>
+                  ) : (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M4 8h16M4 16h16"
+                      />
+                    </svg>
+                  )}
+                </button>
+              </div>
+            </div>
+
+            <div
+              className={`flex lg:flex lg:items-center justify-center lg:justify-between  w-full lg:w-auto transition-transform duration-300 ease-in-out 
+              ${isOpen ? "block  " : "hidden"} lg:block`}
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
-          </div>
-          <div
-            tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[100] p-2  shadow bg-base-100 rounded-box w-52"
-          >
-            <NavLink
-              to="/"
-              className="btn btn-ghost border-2 border-gray-300 hover:bg-black hover:text-white "
-            >
-              Home
-            </NavLink>
-           
-            {user && (
-              <>
-                 <NavLink
-            to="/profile"
-            className="btn btn-ghost border-2 border-gray-300 hover:bg-black hover:text-white"
-          >
-            Profile
-          </NavLink>
-              
-              <NavLink
-            to="/updateProfile"
-            className="btn btn-ghost border-2 border-gray-300 hover:bg-black hover:text-white"
-          >
-            Update-Profile
-          </NavLink>
-            
-              <NavLink
-                to="/blog"
-                className="btn btn-ghost border-2  border-gray-300 hover:bg-black hover:text-white"
-              >
-                Blogs
-              </NavLink>
-              </>
-            )}
-            
-          </div>
-        </div>
-        <Link
-          to="/"
-          className="btn  btn-ghost text-base md:text-xl lg:text-3xl mr-0 font-bold animate__animated animate__swing animate__delay-0.5s"
-        >
-          <span className="text-pink-700">RELAINS</span> RELUX
-        </Link>
-      </div>
-      <div className="navbar-center hidden space-x-2  lg:flex">
-        <NavLink
-          to="/"
-          className="btn btn-ghost border-2 border-gray-300 hover:bg-black hover:text-white "
-        >
-          Home
-        </NavLink>
-        <NavLink
-          to="/gellary"
-          className="btn btn-ghost border-2 border-gray-300 hover:bg-black hover:text-white "
-        >
-          Gellary
-        </NavLink>
-        {user && (
-          <NavLink
-            to="/blog"
-            className="btn btn-ghost border-2  border-gray-300 hover:bg-black hover:text-white"
-          >
-            Blogs
-          </NavLink>
-        )}
-        
-      </div>
-      <div className="navbar-end ">
-     
-        {user ? (
-          <div  className="flex items-center ">
-            <div className="dropdown dropdown-end">
-          <div tabIndex={0} role="button">
-          <div
-              tabIndex={0}
-              role="button"
-              className=" border rounded-full border-gray-300  avatar hover:tooltip tooltip-open"
-              data-tip={user ? user.displayName : "Invalid Name"}
-            >
-              <div className=" rounded-full w-9 md:w-12  ">
-                <img  alt="" src={user.photoURL} />
+              <div className=" flex flex-col  lg:flex-row lg:items-center lg:mx-8 lg:space-x-2 ">
+                <NavLink
+                  to="/"
+                  className="btn btn-sm  border-b-2 border-gray-300 hover:bg-black hover:text-white "
+                >
+                  Home
+                </NavLink>
+                <NavLink
+                  to="/listing"
+                  className="btn  btn-sm border-2 border-gray-300 hover:bg-black hover:text-white "
+                >
+                  Pet Listing
+                </NavLink>
+                <NavLink
+                  to="/campaign"
+                  className="btn btn-sm border-2 border-gray-300 hover:bg-black hover:text-white "
+                >
+                  Donation Campaigns
+                </NavLink>
+                <NavLink
+                  to="/contract"
+                  className="btn btn-sm border-2 border-gray-300 hover:bg-black hover:text-white "
+                >
+                  Contract
+                </NavLink>
+              </div>
+
+              <div className="flex items-center mt-4 lg:mt-0">
+                {user ? (
+                  <div className="flex items-center ">
+                    <div className="dropdown dropdown-end">
+                      <div tabIndex={0} role="button">
+                        <div
+                          tabIndex={0}
+                          role="button"
+                          className=" border rounded-full border-gray-300 z-[110]  avatar"
+                        >
+                          <div className=" rounded-full w-9 md:w-12  ">
+                            <img alt="" src={user?.photoURL || ""} />
+                          </div>
+                        </div>
+                      </div>
+                      <ul
+                        tabIndex={0}
+                        className="dropdown-content  flex  z-[100] menu p-2 gap-2 shadow bg-base-100 rounded-box w-64"
+                      >
+                        <div className="flex justify-center">
+                          <img
+                            className="rounded-full w-20 text-center"
+                            src={user?.photoURL || ""}
+                            alt=""
+                          />
+                        </div>
+                        <li className="mx-auto text-xl font-bold ">
+                          ----- {user?.displayName} -----
+                        </li>
+                        <li>
+                          <NavLink
+                            to="/profile"
+                            className="btn btn-sm btn-ghost border-2  border-gray-300 hover:bg-black hover:text-white"
+                          >
+                            Profile
+                          </NavLink>
+                        </li>
+                        <li>
+                          <NavLink
+                            to="/dashBoard"
+                            className="btn btn-sm btn-ghost border-2  border-gray-300 hover:bg-black hover:text-white"
+                          >
+                            DashBoard
+                          </NavLink>
+                        </li>
+
+                        <button
+                          onClick={Logout}
+                          className="btn btn-sm btn-ghost border-2  border-gray-300 hover:bg-black hover:text-white"
+                        >
+                          Logout
+                        </button>
+                      </ul>
+                    </div>
+                  </div>
+                ) : (
+                  <Link
+                    to="/login"
+                    className="rounded-md btn-sm  m-1 overflow-hidden relative group cursor-pointer border-2 font-medium border-[#ff4880] text-[#ff4880] hover:text-white"
+                  >
+                    <span className="absolute w-64 h-0 transition-all duration-300 origin-center rotate-45 -translate-x-20 bg-[#ff4880] top-1/2 group-hover:h-64 group-hover:-translate-y-32 ease"></span>
+                    <span className="relative text-[#ff4880] transition duration-300 group-hover:text-white ease">
+                      Login
+                    </span>
+                  </Link>
+                )}
               </div>
             </div>
           </div>
-          <ul
-            tabIndex={0}
-            className="dropdown-content hidden lg:flex  z-[100] menu p-2 gap-2 shadow bg-base-100 rounded-box w-52"
-          >
-            <li>
-            <NavLink
-          to="/profile"
-          className="btn btn-ghost border-2  border-gray-300 hover:bg-black hover:text-white"
-        >
-          Profile
-        </NavLink>
-            </li>
-            <li>
-            <NavLink
-          to="/updateProfile"
-          className="btn btn-ghost border-2  border-gray-300 hover:bg-black hover:text-white"
-        >
-          Update-Profile
-        </NavLink>
-            </li>
-          </ul>
         </div>
-            <button
-              onClick={Logout}
-              className="rounded-md btn-sm md:btn-md lg:px-3.5 lg:py-2 m-1 overflow-hidden relative group cursor-pointer border-2 p-1 font-medium border-pink-700 text-pink-700 hover:text-white"
-            >
-              <span className="absolute w-64 h-0 transition-all duration-300 origin-center rotate-45 -translate-x-20 bg-pink-700 top-1/2 group-hover:h-64 group-hover:-translate-y-32 ease"></span>
-              <span className="relative  text-pink-700 transition duration-300 group-hover:text-white ease">
-                Logout
-              </span>
-            </button>
-          </div>
-        ) : (
-          <Link data-aos="fade-left" data-aos-duration='1000'
-            to="/login"
-            className="rounded-md btn-sm md:btn-md p-1 md:p-2 lg:px-3.5 lg:py-2 m-1 overflow-hidden relative group cursor-pointer border-2 font-medium border-[#ad1199] text-[#ad1199] hover:text-white"
-          >
-            <span className="absolute w-64 h-0 transition-all duration-300 origin-center rotate-45 -translate-x-20 bg-[#ad1199] top-1/2 group-hover:h-64 group-hover:-translate-y-32 ease"></span>
-            <span className="relative text-[#ad1199] transition duration-300 group-hover:text-white ease">
-              Login
-            </span>
-          </Link>
-        )}
-      </div>
-    </nav>
+      </nav>
+    </div>
   );
 };
 
